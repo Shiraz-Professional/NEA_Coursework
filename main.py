27011331f784512f98fea59da65ee8448595e377
Shiraz-Professional <166607168+Shiraz-Professional@users.noreply.github.com>
2025-09-23 12:57:53 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-import math
+import random
 
-print("hello world")
+InvisibleHandFile = open("InvisibleHand.csv","r")
+InvisibleHandScenarios = InvisibleHandFile.readlines() #Holds the scenarios for the Invisible Hand Games.
+InvisibleHandFile.close()
+
+PrisonersDilemmaFile = open("PrisonersDilemma.csv")
+PrisonersDilemmaScenarios = PrisonersDilemmaFile.readlines() #Holds the scenarios for the Prisoners' Dilemma Games.
+PrisonersDilemmaFile.close()
+
+PNEquilibriumFile = open("PureNashEquilibrium.csv")
+PureNashScenarios = PNEquilibriumFile.readlines() #Holds the scenarios for Pure Nash Equilibria Games.
+PNEquilibriumFile.close()
+
+MNEquilibriumFile = open("MixedNashEquilibrium.csv")
+MixedNashScenarios = MNEquilibriumFile.readlines() #Holds the scenarios for Mixed Nash Equilibria Games. 
+MNEquilibriumFile.close()
+
+#Extracting data from the Invisible Hand Scenario
+
+RandomScenario = random.randint(1,25)
