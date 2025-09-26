663fa6ba9dc6e8144c8eec2cae5e3052d5df53d2
Shiraz-Professional <166607168+Shiraz-Professional@users.noreply.github.com>
2025-09-26 18:55:16 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Game of Game Theory: NEA Coursework. 
 import random
 
 InvisibleHandFile = open("InvisibleHand.csv","r")
@@ -16,6 +17,15 @@ MNEquilibriumFile = open("MixedNashEquilibrium.csv")
 MixedNashScenarios = MNEquilibriumFile.readlines() #Holds the scenarios for Mixed Nash Equilibria Games. 
 MNEquilibriumFile.close()
 
-#Extracting data from the Invisible Hand Scenario
+#This function retrieves a particular feature from a scenario
+def ScenarioSplitter(someScenarios,fieldName):
+    indexDictionary = {"scenarioNumber":0,"scenarioText":1,"ChoiceA":2,"ChoiceB":3,"ChoiceC":4,"ChoiceD":5,"Payoff1":6,"Payoff2":7,"Payoff3":8,"Payoff4":9,"Payoff5":10,"Payoff6":11,"Payoff7":12,"Payoff8":13}
+    index = indexDictionary.get(fieldName)
+    RandomScenario = random.randint(1,26)
+    someScenario = someScenarios[RandomScenario].split(",")  
+    return someScenario[index]
 
-RandomScenario = random.randint(1,25)
+#Tests for ScenarioSplitter
+#print(ScenarioSplitter(InvisibleHandScenarios,"scenarioNumber"))
+#print(ScenarioSplitter(InvisibleHandScenarios,"scenarioText"))
+#print(ScenarioSplitter(InvisibleHandScenarios,"ChoiceB"))
